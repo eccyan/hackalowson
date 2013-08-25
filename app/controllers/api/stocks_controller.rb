7f675933b304474870ba7f33d568bf3692895cb5
sato_daisuke <sato_daisuke@cyberagent.co.jp>
2013-08-25 13:18:23 +0900

    added create apis

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -1,4 +1,5 @@
 class Api::StocksController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   before_action :set_stock, only: [:show, :edit, :update, :destroy]
 
   # GET /stocks
@@ -52,8 +53,8 @@ class Api::StocksController < ApplicationController
 
     respond_to do |format|
       if @stock.save
-        format.html { redirect_to @stock, notice: 'Stock was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @stock }
+        format.html { redirect_to [:api, @stock], notice: 'Stock was successfully created.' }
+        format.json { render action: 'show', status: :created, location: [:api, @stock] }
       else
         format.html { render action: 'new' }
         format.json { render json: @stock.errors, status: :unprocessable_entity }
@@ -66,7 +67,7 @@ class Api::StocksController < ApplicationController
   def update
     respond_to do |format|
       if @stock.update(stock_params)
-        format.html { redirect_to @stock, notice: 'Stock was successfully updated.' }
+        format.html { redirect_to [:api, @stock], notice: 'Stock was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -80,7 +81,7 @@ class Api::StocksController < ApplicationController
   def destroy
     @stock.destroy
     respond_to do |format|
-      format.html { redirect_to stocks_url }
+      format.html { redirect_to api_stocks_url }
       format.json { head :no_content }
     end
   end
@@ -119,6 +120,9 @@ class Api::StocksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def stock_params
-      params.require(:stock).permit(:id, :storeId, :odenId, :number, :available, :entryTime, :recordedTime)
+      @stock_params = params.require(:stock).permit(:id, :odenId, :number, :available, :storeId, :entryTime, :recordedTime)
+      @stock_params[:storeId] = params[:store_id] unless @stock_params[:storeId]
+      @stock_params[:entryTime] = Time.now
+      @stock_params
     end
 end
